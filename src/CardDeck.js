a9fb94a232001527ec6b518815a47967f56158a4
Jasbir <jasbir@sahusoft.com>
2017-09-18 17:47:27 +0530

    SizeFix

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -40,7 +40,7 @@ export default class ScrollSwagger extends Component {
       onPanResponderMove: (dx, dy) => {
         // console.log(this.state.diff, this.state.noOfItems * 100);
         if (
-          this.state.diff > this.state.noOfItems * 140 + 30 &&
+          this.state.diff > this.state.noOfItems * 120 + 30 &&
           dy.dy - this.state.scrollY > 0
         ) {
           // console.log("OverScrolling");
@@ -68,25 +68,25 @@ export default class ScrollSwagger extends Component {
   }
   renderCards(index) {
     let scrollArray = [];
-    let max = this.state.noOfItems * 140;
-    scrollArray.push(max - 140 * index - 1);
-    scrollArray.push(max - 140 * index);
-    scrollArray.push(max - 140 * index + 400);
-    scrollArray.push(max - 140 * index + 401);
+    let max = this.state.noOfItems * 120;
+    scrollArray.push(max - 120 * index - 1);
+    scrollArray.push(max - 120 * index);
+    scrollArray.push(max - 120 * index + 400);
+    scrollArray.push(max - 120 * index + 401);
     let top = 0;
     let widFac = 1;
     switch (index) {
       case this.state.noOfItems - 1:
         top = 150;
-        widFac = 1.1;
+        widFac = 1.04;
         break;
       case this.state.noOfItems - 2:
         top = 100;
-        widFac = 1.066;
+        widFac = 1.03;
         break;
       case this.state.noOfItems - 3:
         top = 50;
-        widFac = 1.033;
+        widFac = 1.02;
         break;
     }
     let movFactor = this.state.ani.interpolate({
@@ -95,7 +95,7 @@ export default class ScrollSwagger extends Component {
     });
     let scaleFactor = this.state.ani.interpolate({
       inputRange: scrollArray,
-      outputRange: [1, 1, 1.3, 1.3]
+      outputRange: [1, 1, 1.1, 1.1]
     });
     return (
       <Animated.View
